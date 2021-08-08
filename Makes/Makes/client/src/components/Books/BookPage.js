cd622502b8155ecf597138eb07efc2dd59344880
Nir <nirolol1994@gmail.com>
2021-08-08 16:01:43 +0300

    questionnarie http

--- Makes/Makes/client/src/components/Books/BookPage.js
+++ Makes/Makes/client/src/components/Books/BookPage.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TurningPoint from '../ReadBookPage/TurningPoint'
 
 import './BookPage.css'
 
 const BookPage = (props) => {
     
+    const [isRecording, setIsRecording] = useState(false);
+    const [isRecordingExist, setIsRecordingExist] = useState(false);
+
     const pageData= props.data;
     const img = pageData.img;
+
+    const recordButtonHandler = () => {
+      setIsRecording(!isRecording);
+      setIsRecordingExist(true);
+    }
 
     return(
     <React.Fragment >
         <div className="book-page">
+          <button onClick={recordButtonHandler}> {isRecording ? 'עצור' : 'הקלט'}</button>
           <img src={require("../../images/KipaAduma.jfif").default}/>
         </div>
 
         <div className="book-page">
         <p>{pageData.text}</p>
+        {isRecordingExist && <audio controls></audio>}
         {pageData.turningPointExist && <TurningPoint data={pageData.turningPointData} onTurningPointChosen={props.onTurningPointChosen}/>}
         </div>
     </React.Fragment>

75d4496eeabc530fc5adbf80f7a308ca38837b20
gabySivan <gabysivan@gamil.com>
2021-08-14 17:53:23 +0300

    add user gallery

--- Makes/Makes/Makes/src/main/java/com/makes/makes/controller/BookCoverController.java
+++ Makes/Makes/Makes/src/main/java/com/makes/makes/controller/BookCoverController.java
@@ -2,12 +2,10 @@ package com.makes.makes.controller;
 
 import com.makes.makes.model.BookCover;
 import com.makes.makes.service.BookCoverService;
+import com.makes.makes.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,13 +17,25 @@ public class BookCoverController {
     private static final Logger log = LoggerFactory.getLogger(BookCoverController.class.getName());
 
     private final BookCoverService bookCoverService;
+    private final CustomBookController customBookController;
+    private final UserService userService;//TODO CHECK IF NEED!
 
-    public BookCoverController(BookCoverService bookCoverService) {
+    public BookCoverController(BookCoverService bookCoverService,CustomBookController customBookController,UserService userService) {
         this.bookCoverService = bookCoverService;
+        this.customBookController = customBookController;
+        this.userService = userService;
     }
 
     @GetMapping("/")
     public List<BookCover> fetchAllBooksCover() {
         return bookCoverService.getAllBookCovers();
+    }
+
+    @GetMapping("/{id}")
+    public BookCover userGallery(@PathVariable String id)
+    {
+        //TODO maybe we can save the user id in customBook.owner
+
+
     }
 }

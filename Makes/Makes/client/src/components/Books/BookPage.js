a692bfe587cbeaadc1275bed6e06c7c8f6f97d51
Nir <nirolol1994@gmail.com>
2021-08-18 19:50:26 +0300

    deleted record

--- Makes/Makes/client/src/components/Books/BookPage.js
+++ Makes/Makes/client/src/components/Books/BookPage.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect, useRef} from 'react';
 import TurningPoint from '../ReadBookPage/TurningPoint'
-import RecordView from '../UI/RecordView'
 
 import './BookPage.css'
 

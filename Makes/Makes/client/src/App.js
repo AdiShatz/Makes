beac3d699790ced48c90096fa3ae96f2b0473f01
Nir <nirolol1994@gmail.com>
2021-08-19 14:43:38 +0300

    post request

--- Makes/Makes/client/src/App.js
+++ Makes/Makes/client/src/App.js
@@ -6,19 +6,6 @@ import GalleryPage from "./components/GalleryPage/GalleryPage";
 import AuthContext, {AuthContextProvider} from "./store/auth-context";
 import AdminPage from "./components/AdminPage/AdminPage";
 
-// const DUMMY_GALLERY_BOOKS = [
-//   {
-//     id: 'b1',
-//     name: 'כיפה אדומה',
-//     coverPhoto: "KipaAduma.jfif"
-//   },
-//   { 
-//       id: 'b4',
-//     name: 'שילגיה',
-//     coverPhoto: "Shilgiya.jpeg"
-//   }
-// ];
-
   const App = () => {
 
     const [page, setPage] = useState("mainPage");
@@ -40,7 +27,36 @@ import AdminPage from "./components/AdminPage/AdminPage";
   };
 
 
-    const galleryBookItemClickedHandler = () => {
+    const galleryBookItemClickedHandler = (bookName) => {
+      let url = "http://localhost:8080/customBooks/readUserBook";
+            fetch(url,
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    userName: localStorage.getItem("userEmail"),
+                    bookName: bookName
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+             })
+            .then(response => {
+                if(response.ok){
+                  console.log("200 OK");
+                    return response.json();
+                }
+                else{
+                    return response.json().then((data)=>{
+                         let errorMessage= 'מצטערים, אירעה שגיאה ';
+                         if(data && data.error && data.error.message){ 
+                         errorMessage = data.error.message; 
+                         }
+                         throw new Error(errorMessage);
+                    });
+                }
+            }).then((data) => { 
+              setBookPages(data.pages);
+            });
 
       setPage("readBookPage");
     };
@@ -76,8 +92,6 @@ import AdminPage from "./components/AdminPage/AdminPage";
                     });
                 }
             }).then((data) => { 
-              console.log("create-book data");
-              console.log(data);
               setBookPages(data.pages);
             });
 

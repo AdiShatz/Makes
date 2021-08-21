da82f6cfae1fa719e8db39f5797da1a1e22796bd
lilachbirnbaum <57711975+lilachbirnbaum@users.noreply.github.com>
2021-08-21 14:31:07 +0300

    css

--- Makes/Makes/client/src/components/CreateBookPage/CreateBookPageContent.js
+++ Makes/Makes/client/src/components/CreateBookPage/CreateBookPageContent.js
@@ -11,7 +11,9 @@ const CreateBookPageContent = (props) => {
         <div id="top-Padding">
                 <div style={{textAlign: "center"}}><label id="create-book-subtitle">מלא את השאלון והסיפור שלך תיכף יהיה מוכן</label></div>
                 <div className="create-book-page-container">
-                    <div className="right-container"><p>הרבה הרבה הסברים על השאלון והסיפור</p></div> 
+                    <div className="right-container"><p>התשובות שתספק לשאלון יעזרו לנו לבנות ספר מותאם אישית,
+שדות אשר לא ימולאו יקבלו את הערך המקורי מהסיפור.
+בסוף מילוי השאלון לחץ על "צור ספר" ותועבר למסך קריאת הספר, שם תוכל לבחור האם ברצונך שהספר ישמר בגלרייה או לא.</p></div> 
                     <div className="left-container"><Questionnaire questions={props.questions} onCreateBook={props.onCreateBook}/> </div>
                 </div>                
         </div>

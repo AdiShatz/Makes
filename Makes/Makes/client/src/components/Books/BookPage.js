a31087ef2e139ba8cb8b30c3d8310334c137ef59
Nir <nirolol1994@gmail.com>
2021-09-03 23:24:08 +0300

    edit box

--- Makes/Makes/client/src/components/Books/BookPage.js
+++ Makes/Makes/client/src/components/Books/BookPage.js
@@ -6,12 +6,29 @@ import './BookPage.css'
 const BookPage = (props) => {
     
     const [pageData, setPageData] = useState(null);
+    const [pageText, setPageText] = useState(null);
+    const [isEditMode, setIsEditMode] = useState(false);
+    const [isEdited, setIsEdited] = useState(false);
+
+    const chosenBookTextInputRef = useRef(null);
+
 
     useEffect(
         () => {
             setPageData(props.data);
         },[props]
     )
+
+    const handleEdit = () =>{
+      setIsEditMode(true);
+      
+    }
+
+    const handleSave = () =>{
+      setIsEditMode(false);
+      setIsEdited(true);
+      setPageText(chosenBookTextInputRef.current.value);
+          }
 
     return(
         <div>
@@ -24,8 +41,17 @@ const BookPage = (props) => {
           </div>
 
           <div className="book-page-container-right">
-              {pageData && <p>{pageData.text}</p>}
+              {!isEditMode && !isEdited && pageData && <p>{pageData.text}</p>}
+
+              {isEditMode && !isEdited && <textarea id="textarea" dir = "rtl" name="textarea" rows="20" cols="100" required ref={chosenBookTextInputRef}>
+              {pageData.text}
+              </textarea>}
+
+              {isEdited && <p>{pageText}</p>}
               {pageData && pageData.turningPointExist && <TurningPoint data={pageData.turningPoint} onTurningPointChosen={props.onTurningPointChosen}/>}
+              <br/>
+             {!isEditMode && <button onClick={handleEdit}>ערוך</button>}
+             {isEditMode && <button onClick={handleSave}>שמור</button>}
         </div>
 
       </div>

bc6e48c8e13f4881ef8cbdbf20fdd609f15dfc3d
Nir <nirolol1994@gmail.com>
2021-09-12 18:23:54 +0300

    Update CreateBookPageContent.js

--- Makes/Makes/client/src/components/CreateBookPage/CreateBookPageContent.js
+++ Makes/Makes/client/src/components/CreateBookPage/CreateBookPageContent.js
@@ -12,7 +12,7 @@ const CreateBookPageContent = (props) => {
                 <div className="create-book-page-container">
                     <div className="right-container"><p>התשובות שתספק לשאלון יעזרו לנו לבנות ספר מותאם אישית,
 שדות אשר לא ימולאו יקבלו את הערך המקורי מהסיפור.
-בסוף מילוי השאלון לחץ על "צור ספר" ותועבר למסך קריאת הספר, שם תוכל לבחור האם ברצונך שהספר ישמר בגלרייה או לא.</p></div> 
+בסוף מילוי השאלון לחץ על "צור ספר" ותועבר למסך קריאת הספר .</p></div> 
                     <div className="left-container"><Questionnaire questions={props.questions} onCreateBook={props.onCreateBook}/> </div>
                 </div>                
         </div>
